6d37d6e142bda79f4bdd4096822e446e9b6aff75
cadupee1464 <cadupee@icloud.com>
2026-01-08 12:50:03 -0800

    completed FastAPI code

--- main.py
+++ main.py
@@ -26,21 +26,24 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
+path = "model/encoder.pkl" # TODO: enter the path for the saved encoder 
 encoder = load_model(path)
 
-path = None # TODO: enter the path for the saved model 
+path = "model/model.pkl" # TODO: enter the path for the saved model 
 model = load_model(path)
 
+scaler = load_model(os.path.join("model", "scaler.pkl"))
+lb = load_model(os.path.join("model", "lb.pkl"))
+
 # TODO: create a RESTful API using FastAPI
-app = None # your code here
+app = FastAPI()
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
-    # your code here
-    pass
+    return {"message": "Welcome to my Census API"}
+    
 
 
 # TODO: create a POST on a different path that does model inference
@@ -64,11 +67,15 @@ async def post_inference(data: Data):
         "sex",
         "native-country",
     ]
-    data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
+    
+    data_processed, _, _, _, _ = process_data(
+    data,
+    categorical_features=cat_features,
+    training=False,
+    encoder=encoder,
+    scaler=scaler,
+    lb=lb
     )
-    _inference = None # your code here to predict the result using data_processed
+
+    _inference = inference(model, data_processed) # your code here to predict the result using data_processed
     return {"result": apply_label(_inference)}
