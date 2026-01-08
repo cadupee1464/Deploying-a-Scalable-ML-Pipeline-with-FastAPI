d3b80e078b4190e4234bae28d0074f2a8833c215
cadupee1464 <cadupee@icloud.com>
2026-01-08 10:36:38 -0800

    Implemented prediction function in ml/model.py

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,7 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-# TODO: add necessary import
+from sklearn.linear_model import LogisticRegression
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -19,8 +19,11 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+    model = LogisticRegression(solver = "saga", random_state = 42)
+
+    model.fit(X_train, y_train)
+
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -50,7 +53,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : object
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -59,8 +62,10 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+
+    preds = model.predict(X)
+
+    return preds
 
 def save_model(model, path):
     """ Serializes model to a file.
